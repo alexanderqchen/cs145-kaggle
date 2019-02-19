d1c07f0a516e2faf6eef068c28f9ec48c8a03aff
Yijing Zhou <yijingz1227@gmail.com>
2019-02-19 01:09:51 +0000

    Add in user feature vectorization
    Details:
    	- in vector_data.py, a new function userFeatureVectorization has been added
    		- this function will read the total number of users in training dataset
    		- for each user:
    			- it will first find his or her preference for types of movies
    			  by averaging the rated_movies' relevances
    			- then the user's average ratings for all the movies he or she rated
    	- the main routine of vector_data.py has been changed. For legacy use, un-comment.

--- src/data/vector_data.py
+++ src/data/vector_data.py
@@ -28,8 +28,42 @@ class DataProcessor():
             movies[i][0] = i + 1
         return movies
 
+    def userFeatureVectorization(self, train_ratings, movies):
+        # accquire the number of users
+        maxNum_users = np.amax(train_ratings['userId'])
+        # The columns are: average relevance per tag
+        users = np.zeros((maxNum_users, 1130))
+        # scale original train_ratings to [-2, 2]
+        train_ratings['rating'] = train_ratings['rating'] - 3
+        # begin vectorization
+        for i in range(1, maxNum_users + 1):
+            user_tuples = train_ratings.loc[train_ratings['userId'] == i]
+            rated_movies = movies[movies['movieId'].isin(user_tuples['movieId'])]
+            average = rated_movies.sum(axis=0) / rated_movies.shape[0]
+            average[0] = i
+            average_rating = user_tuples['rating'].sum() / user_tuples.shape[0]
+            users[i - 1] = np.append(np.array(average), average_rating)
+        return users
+
 
 if __name__ == '__main__':
+    ps = DataProcessor()
+    # Accquire files
+    train_ratings = ps.getDataFrame('../../csv/train_ratings.csv')
+    movies = ps.getDataFrame('../../csv/movies_vectors.csv')
+    # Transofrorming userId to vectors
+    users = ps.userFeatureVectorization(train_ratings, movies)
+    # Creating appropirate headers
+    names = ['userId']
+    for i in range(1, 1129):
+        tagId = 'tag' + str(i)
+        names.append(tagId)
+    names.append('average_rating')
+    # Export dataframe to csv files
+    pd.DataFrame(users).to_csv("users.csv", header=names, index=False)
+
+    # Legacy code: generates movies_vectors.csv
+    '''
     # Create a DataProcessor instance to access attribute functions
     ps = DataProcessor()
     # Accessing files
@@ -43,3 +77,4 @@ if __name__ == '__main__':
         names.append(tagId)
     # Export dataframe to csv files
     pd.DataFrame(movies).to_csv("movies_vectors.csv", header=names, index=False)
+    '''
