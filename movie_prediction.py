35585b859f48d05b6f5fc0e56ed770ebafd214a2
Michael Wu <cheeserules43@gmail.com>
2019-03-12 23:52:57 +0000

    added sampling, validation, random forest regressor and lowered memory

--- movie_prediction.py
+++ movie_prediction.py
@@ -2,8 +2,9 @@ import os
 import math
 
 from pyspark import SparkConf, SparkContext
+from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.regression import LinearRegression, LinearRegressionModel
+from pyspark.ml.regression import LinearRegression, LinearRegressionModel, RandomForestRegressor
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import avg, col, udf
 from pyspark.sql.types import FloatType
@@ -29,6 +30,12 @@ def get_initial_dfs(context, saved_initial_dfs):
             col("userId").cast("int").alias("userId"),
             col("movieId").cast("int").alias("movieId"),
             col("rating").cast("float").alias("rating"))
+        val_ratings = context.read.format("csv").option("header", "true").load(
+            os.path.join(script_dir, data_dir + "val_ratings.csv"))
+        val_ratings = train_ratings.select(
+            col("userId").cast("int").alias("userId"),
+            col("movieId").cast("int").alias("movieId"),
+            col("rating").cast("float").alias("rating"))
         test_ratings = context.read.format("csv").option("header", "true").load(
             os.path.join(script_dir, data_dir + "test_ratings.csv"))
         test_ratings = test_ratings.select(
@@ -51,23 +58,30 @@ def get_initial_dfs(context, saved_initial_dfs):
         movie_vectors.write.parquet(os.path.join(space_dir, parquet_dir + "movie_vectors.parquet"))
         user_vectors.write.parquet(os.path.join(space_dir, parquet_dir + "user_vectors.parquet"))
         train_ratings.write.parquet(os.path.join(space_dir, parquet_dir + "train_ratings.parquet"))
+        val_ratings.write.parquet(os.path.join(space_dir, parquet_dir + "val_ratings.parquet"))
         test_ratings.write.parquet(os.path.join(space_dir, parquet_dir + "test_ratings.parquet"))
     else:
         movie_vectors = context.read.parquet(os.path.join(space_dir, parquet_dir + "movie_vectors.parquet"))
         user_vectors = context.read.parquet(os.path.join(space_dir, parquet_dir + "user_vectors.parquet"))
         train_ratings = context.read.parquet(os.path.join(space_dir, parquet_dir + "train_ratings.parquet"))
+        val_ratings = context.read.parquet(os.path.join(space_dir, parquet_dir + "val_ratings.parquet"))
         test_ratings = context.read.parquet(os.path.join(space_dir, parquet_dir + "test_ratings.parquet"))
 
-    return movie_vectors, user_vectors, train_ratings, test_ratings
+    return movie_vectors, user_vectors, train_ratings, val_ratings, test_ratings
 
 
-def calculate_paired_vectors(context, movie, user, train, test, saved_paired_vectors):
+def calculate_paired_vectors(context, movie, user, train, val, test, saved_paired_vectors):
     if not saved_paired_vectors:
         movie_alias = movie.select("movieId", *((col(c)).alias("movie_" + c) for c in movie.columns[1:]))
         user_alias = user.select("userId", *((col(c)).alias("user_" + c) for c in user.columns[1:]))
 
         print("Calculating training vector.")
-        train_vec = train.join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
+        train_vec = train.sample(False, 0.001).join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
+            "rating",
+            *((col("movie_" + c) * col("user_" + c)).alias(c) for c in movie.columns[1:]))
+
+        print("Calculating validation vector.")
+        val_vec = val.sample(False, 0.0001).join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
             "rating",
             *((col("movie_" + c) * col("user_" + c)).alias(c) for c in movie.columns[1:]))
 
@@ -78,24 +92,31 @@ def calculate_paired_vectors(context, movie, user, train, test, saved_paired_vec
 
         print("Saving parquets.")
         train_vec.write.parquet(os.path.join(space_dir, parquet_dir + "train_vec.parquet"))
+        val_vec.write.parquet(os.path.join(space_dir, parquet_dir + "val_vec.parquet"))
         test_vec.write.parquet(os.path.join(space_dir, parquet_dir + "test_vec.parquet"))
     else:
         train_vec = context.read.parquet(os.path.join(space_dir, parquet_dir + "train_vec.parquet"))
+        val_vec = context.read.parquet(os.path.join(space_dir, parquet_dir + "val_vec.parquet"))
         test_vec = context.read.parquet(os.path.join(space_dir, parquet_dir + "test_vec.parquet"))
 
-    return train_vec, test_vec
+    return train_vec, val_vec, test_vec
 
 
-def train_model(saved_trained_model, train):
+def train_model(saved_trained_model, train, val):
     if not saved_trained_model:
         assembler = VectorAssembler(
             inputCols=[c for c in train.columns[1:]],
             outputCol="features",
             handleInvalid="keep")
 
-        lr = LinearRegression(labelCol="rating", featuresCol="features")
+        lr = RandomForestRegressor(labelCol="rating")
         print("Training model...")
         model = lr.fit(assembler.transform(train))
+        cutoff_udf = udf(cutoff, FloatType())
+        val = model.transform(assembler.transform(val)).select(
+            "rating",
+            cutoff_udf("prediction").alias("prediction"))
+        print("Evaluation: ", RegressionEvaluator(labelCol="rating").evaluate(val))
         model.save(os.path.join(space_dir, models_dir + "movies.model"))
     else:
         model = LinearRegressionModel.load(os.path.join(space_dir, models_dir + "movies.model"))
@@ -132,17 +153,16 @@ def main(context):
     saved_paired_vectors = True
     saved_trained_model = True
 
-    movie_vectors, user_vectors, train_ratings, test_ratings = get_initial_dfs(context, saved_initial_dfs)
-    train_vec, test_vec = calculate_paired_vectors(context, movie_vectors, user_vectors, train_ratings,
-                                                   test_ratings, saved_paired_vectors)
-    model = train_model(saved_trained_model, train_vec)
+    movie_vectors, user_vectors, train_ratings, val_ratings, test_ratings = get_initial_dfs(context, saved_initial_dfs)
+    train_vec, val_vec, test_vec = calculate_paired_vectors(context, movie_vectors, user_vectors, train_ratings,
+                                                   val_ratings, test_ratings, saved_paired_vectors)
+    model = train_model(saved_trained_model, train_vec, val_vec)
     predict_scores(model, test_vec)
 
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS145 Project")
-    conf = conf.setMaster("local[*]").set("spark.executor.memory", "128G").set("spark.driver.memory", "768G").set(
-        "spark.local.dir", "/tmp")
+    conf = conf.setMaster("local[*]").set("spark.executor.memory", "32G").set("spark.driver.memory", "32G")
     sc = SparkContext(conf=conf)
     sql_context = SQLContext(sc)
     main(sql_context)
