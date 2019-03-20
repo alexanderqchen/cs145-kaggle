daa9f5edf9fc4c640dc68ab4eadeee0e7a6a09a8
Michael Wu <cheeserules43@gmail.com>
2019-03-20 07:50:43 +0000

    added pca with pandas

--- generate_test_and_train_data.py
+++ generate_test_and_train_data.py
@@ -1,9 +1,10 @@
 import os
+import pandas as pd
 
 from common import script_dir, parquet_dir, initSpark
 
 from pyspark.sql.functions import col
-from pyspark.ml.feature import PCA, VectorAssembler
+from sklearn.decomposition import IncrementalPCA
 
 def main():
     context = initSpark()
@@ -18,55 +19,54 @@ def main():
     user_alias = user.select("userId", *((col(c)).alias("user_" + c) for c in user.columns[1:]))
 
     print("Calculating training vector....")
-    train_vec = train.sample(False, 0.1).join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
+    train_vec = train.join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
         "rating",
         *((col("movie_" + c) * col("user_" + c)).alias(c) for c in movie.columns[1:]))
     print("Calculating validation vector...")
-    val_vec = val.sample(False, 0.01).join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
+    val_vec = val.join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
         "rating",
         *((col("movie_" + c) * col("user_" + c)).alias(c) for c in movie.columns[1:]))
     print("Calculating test vector....")
     test_vec = test.join(movie_alias, "movieId", "left_outer").join(user_alias, "userId", "left_outer").select(
         "Id",
-        *((col("movie_" + c) * col("user_" + c)).alias(c) for c in movie.columns[1:]))
+        *((col("movie_" + c) * col("user_" + c)).alias(c) for c in movie.columns[1:])).orderBy("Id")
 
-    assembler = VectorAssembler(
-        inputCols=[c for c in movie.columns[1:]],
-        outputCol="features",
-        handleInvalid="keep")
+    print("Converting training data to pandas...")
+    train_vec = train_vec.toPandas().fillna(0)
+    train_x = train_vec.drop("rating", axis=1)
+    train_y = train_vec["rating"]
+    print("Converting validation data to pandas...")
+    val_vec = val_vec.toPandas().fillna(0)
+    val_x = val_vec.drop("rating", axis=1)
+    val_y = val_vec["rating"]
+    print("Converting test data to pandas...")
+    test_vec = test_vec.toPandas().fillna(0)
+    test_x = test_vec.drop("Id", axis=1)
+    test_id = test_vec["Id"]
 
-    print("Adding training feature column...")
-    train_vec = assembler.transform(train_vec)
-    print("Adding validation feature column...")
-    val_vec = assembler.transform(val_vec)
-    print("Adding test feature column...")
-    test_vec = assembler.transform(test_vec)
-
-    pca = PCA(k=100, inputCol="features", outputCol="pca_features")
+    pca = IncrementalPCA(n_components=20)
     print("Fitting PCA model...")
-    model = pca.fit(train_vec)
+    model = pca.fit(train_x)
     print("Reducing training dimensionality...")
-    train_vec = model.transform(train_vec).rdd.map(extractWithRating).toDF(["rating"])
+    train_vec = pd.DataFrame(model.transform(train_x)).astype("float32")
+    train_vec.insert(0, "rating", train_y)
+    train_vec.columns = train_vec.columns.astype(str)
     print("Reducing validation dimensionality...")
-    val_vec = model.transform(val_vec).rdd.map(extractWithRating).toDF(["rating"])
+    val_vec = pd.DataFrame(model.transform(val_x)).astype("float32")
+    val_vec.insert(0, "rating", val_y)
+    val_vec.columns = val_vec.columns.astype(str)
     print("Reducing test dimensionality...")
-    test_vec = model.transform(test_vec).rdd.map(extractWithId).toDF(["Id"])
+    test_vec = pd.DataFrame(model.transform(test_x)).astype("float32")
+    test_vec.insert(0, "Id", test_id)
+    test_vec.columns = test_vec.columns.astype(str)
 
     print("Saving train vector...")
-    train_vec.write.parquet(os.path.join(script_dir, parquet_dir + "train_vec.parquet"))
+    train_vec.to_parquet(os.path.join(script_dir, parquet_dir + "train_vec.parquet"))
     print("Saving validation vector...")
-    val_vec.write.parquet(os.path.join(script_dir, parquet_dir + "val_vec.parquet"))
+    val_vec.to_parquet(os.path.join(script_dir, parquet_dir + "val_vec.parquet"))
     print("Saving test vector...")
-    test_vec.write.parquet(os.path.join(script_dir, parquet_dir + "test_vec.parquet"))
+    test_vec.to_parquet(os.path.join(script_dir, parquet_dir + "test_vec.parquet"))
     print("Done.")
-
-
-def extractWithRating(row):
-    return (row.rating, ) + tuple(row.pca_features.toArray().tolist())
-
-
-def extractWithId(row):
-    return (row.Id, ) + tuple(row.pca_features.toArray().tolist())
 
 
 if __name__ == "__main__":
