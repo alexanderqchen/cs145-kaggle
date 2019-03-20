86c7ef1764be30ec5ca731b36ffb4b2b25191e4c
Michael Wu <cheeserules43@gmail.com>
2019-03-20 16:31:12 +0000

    moved pca into different file

--- generate_test_and_train_data.py
+++ generate_test_and_train_data.py
@@ -1,8 +1,6 @@
 import os
 
-import pandas as pd
 from pyspark.sql.functions import col
-from sklearn.decomposition import IncrementalPCA
 
 from common import script_dir, parquet_dir, init_spark
 
@@ -32,41 +30,12 @@ def main():
         "Id",
         *((col("movie_" + c) * col("user_" + c)).alias(c) for c in movie.columns[1:])).orderBy("Id")
 
-    print("Converting training data to pandas...")
-    train_vec = train_vec.toPandas().fillna(0)
-    train_x = train_vec.drop("rating", axis=1)
-    train_y = train_vec["rating"]
-    print("Converting validation data to pandas...")
-    val_vec = val_vec.toPandas().fillna(0)
-    val_x = val_vec.drop("rating", axis=1)
-    val_y = val_vec["rating"]
-    print("Converting test data to pandas...")
-    test_vec = test_vec.toPandas().fillna(0)
-    test_x = test_vec.drop("Id", axis=1)
-    test_id = test_vec["Id"]
-
-    pca = IncrementalPCA(n_components=20)
-    print("Fitting PCA model...")
-    model = pca.fit(train_x)
-    print("Reducing training dimensionality...")
-    train_vec = pd.DataFrame(model.transform(train_x)).astype("float32")
-    train_vec.insert(0, "rating", train_y)
-    train_vec.columns = train_vec.columns.astype(str)
-    print("Reducing validation dimensionality...")
-    val_vec = pd.DataFrame(model.transform(val_x)).astype("float32")
-    val_vec.insert(0, "rating", val_y)
-    val_vec.columns = val_vec.columns.astype(str)
-    print("Reducing test dimensionality...")
-    test_vec = pd.DataFrame(model.transform(test_x)).astype("float32")
-    test_vec.insert(0, "Id", test_id)
-    test_vec.columns = test_vec.columns.astype(str)
-
-    print("Saving train vector...")
-    train_vec.to_parquet(os.path.join(script_dir, parquet_dir + "train_vec.parquet"))
+    print("Saving training vector...")
+    train_vec.write.parquet(os.path.join(script_dir, parquet_dir + "train_vec_spark.parquet"))
     print("Saving validation vector...")
-    val_vec.to_parquet(os.path.join(script_dir, parquet_dir + "val_vec.parquet"))
+    val_vec.write.parquet(os.path.join(script_dir, parquet_dir + "val_vec_spark.parquet"))
     print("Saving test vector...")
-    test_vec.to_parquet(os.path.join(script_dir, parquet_dir + "test_vec.parquet"))
+    test_vec.write.parquet(os.path.join(script_dir, parquet_dir + "test_vec_spark.parquet"))
     print("Done.")
 
 
---
+++ run_pca.py
@@ -0,0 +1,48 @@
+import os
+
+import pandas as pd
+from sklearn.decomposition import IncrementalPCA
+
+from common import script_dir, parquet_dir
+
+def main():
+    print("Loading training data...")
+    train_vec = pd.read_parquet(os.path.join(script_dir, parquet_dir + "train_vec_spark.parquet")).fillna(0)
+    train_x = train_vec.drop("rating", axis=1)
+    train_y = train_vec["rating"]
+    print("Loading validation data...")
+    val_vec = pd.read_parquet(os.path.join(script_dir, parquet_dir + "val_vec_spark.parquet")).fillna(0)
+    val_x = val_vec.drop("rating", axis=1)
+    val_y = val_vec["rating"]
+    print("Loading test data...")
+    test_vec = pd.read_parquet(os.path.join(script_dir, parquet_dir + "test_vec_spark.parquet")).fillna(0)
+    test_x = test_vec.drop("Id", axis=1)
+    test_id = test_vec["Id"]
+
+    pca = IncrementalPCA(n_components=20)
+    print("Fitting PCA model...")
+    model = pca.fit(train_x)
+    print("Reducing training dimensionality...")
+    train_vec = pd.DataFrame(model.transform(train_x)).astype("float32")
+    train_vec.insert(0, "rating", train_y)
+    train_vec.columns = train_vec.columns.astype(str)
+    print("Reducing validation dimensionality...")
+    val_vec = pd.DataFrame(model.transform(val_x)).astype("float32")
+    val_vec.insert(0, "rating", val_y)
+    val_vec.columns = val_vec.columns.astype(str)
+    print("Reducing test dimensionality...")
+    test_vec = pd.DataFrame(model.transform(test_x)).astype("float32")
+    test_vec.insert(0, "Id", test_id)
+    test_vec.columns = test_vec.columns.astype(str)
+
+    print("Saving training vector...")
+    train_vec.to_parquet(os.path.join(script_dir, parquet_dir + "train_vec.parquet"))
+    print("Saving validation vector...")
+    val_vec.to_parquet(os.path.join(script_dir, parquet_dir + "val_vec.parquet"))
+    print("Saving test vector...")
+    test_vec.to_parquet(os.path.join(script_dir, parquet_dir + "test_vec.parquet"))
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
